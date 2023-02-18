9c438158d1e34f1313375f38fe063eea3cb2962f
Zeynep Sena Kaya <101938162+zeynepsenakaya@users.noreply.github.com>
2023-02-18 18:56:41 +0300

    Add files via upload

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zekaya <zekaya@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/18 14:13:21 by zekaya            #+#    #+#             */
+/*   Updated: 2023/02/18 16:08:10 by zekaya           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_get_line(char *str)
+{
+	int		i;
+	char	*line;
+	int		new_line;
+
+	i = 0;
+	new_line = ft_strchr(str, '\n');
+	if (!str[i])
+		return (0);
+	while (str[i] != '\0' && str[i] != '\n')
+		i++;
+	line = malloc(sizeof(char) * i + 1 + new_line);
+	if (!line)
+		return (0);
+	i = 0;
+	while (str[i] != '\0' && str[i] != '\n')
+	{
+		line[i] = str[i];
+		i++;
+	}
+	if (str[i] == '\n')
+		line[i++] = '\n';
+	line[i] = '\0';
+	return (line);
+}
+
+char	*ft_get_new_str(char *str)
+{
+	int		i;
+	int		j;
+	char	*new_line;
+
+	i = 0;
+	j = ft_strlen(str);
+	while (str[i] != '\n' && str[i] != '\0')
+		i++;
+	if (str[i] == '\0')
+	{
+		free(str);
+		return (0);
+	}
+	new_line = malloc(sizeof(char) * (j - i) + 1);
+	if (!new_line)
+		return (0);
+	j = 0;
+	i++;
+	while (str[i] != '\0')
+		new_line[j++] = str[i++];
+	new_line[j] = '\0';
+	free(str);
+	return (new_line);
+}
+
+char	*ft_get_read(int fd, char *str)
+{
+	int		count;
+	char	*now_str;
+
+	now_str = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!now_str)
+		return (0);
+	count = 1;
+	while (!ft_strchr(str, '\n') && count != 0)
+	{
+		count = read(fd, now_str, BUFFER_SIZE);
+		if (count == -1)
+		{
+			free(now_str);
+			return (0);
+		}
+		now_str[count] = '\0';
+		str = ft_strjoin(str, now_str);
+	}
+	free(now_str);
+	return (str);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*str;
+	char		*line;
+
+	if (fd == -1 || BUFFER_SIZE <= 0)
+	{
+		return (0);
+	}
+	str = ft_get_read(fd, str);
+	if (!str)
+		return (0);
+	line = ft_get_line(str);
+	str = ft_get_new_str(str);
+	return (line);
+}
---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zekaya <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/18 14:13:44 by zekaya            #+#    #+#             */
+/*   Updated: 2023/02/18 14:13:50 by zekaya           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_get_read(int fd, char *str);
+int		ft_strchr(char *str, int c);
+int		ft_strlen(char *str);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_get_line(char *str);
+char	*ft_get_new_str(char *str);
+
+#endif
